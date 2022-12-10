4ef9846de4eb6ed307769aceecccf1247b49cd4d
0xbm <0xmb@protonmail.com>
2022-12-10 12:43:34 +0100

    constant movement #2

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,6 +18,7 @@ class AlienInvasion:
     def run_game(self):
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -26,7 +27,10 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
