1a8d71a10c87c3775b2f3b987cdecda19d354e88
0xbm <0xmb@protonmail.com>
2022-12-19 14:06:09 +0100

    refactor

--- settings.py
+++ settings.py
@@ -18,7 +18,8 @@ class Settings:
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
-        # self.bullet_speed = 1.5
-        # self.alien_speed = 1.0
-        # self.fleet_direction = 1
+        self.bullet_speed = 1.5
+        self.alien_speed = 1.0
         self.ship_speed = 1.5
+
+        self.fleet_direction = 1
