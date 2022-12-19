ef9c182d82e4778ce695c9ccea4a904777313ae5
0xbm <0xmb@protonmail.com>
2022-12-19 17:04:28 +0100

    commit

--- alien_invasion.py
+++ alien_invasion.py
@@ -99,6 +99,7 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
 
             self.aliens.empty()
             self.bullets.empty()
@@ -153,6 +154,11 @@ class AlienInvasion:
     def _check_bullet_alien_collisions(self):
 
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if collisions:
+            for alien in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+                self.sb.prep_score()
+
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
