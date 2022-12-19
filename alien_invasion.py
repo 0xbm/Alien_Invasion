b2784f37acdddf2a55827847f3b6597a0f201eb8
0xbm <0xmb@protonmail.com>
2022-12-19 14:12:22 +0100

    reset speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -94,6 +94,7 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
 
@@ -165,6 +166,11 @@ class AlienInvasion:
             self.play_button.draw_button()
 
         pygame.display.flip()
+
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+            self.settings.increase_speed()
 
     def _fire_bullet(self):
         if len(self.bullets) < self.settings.bullets_allowed:
