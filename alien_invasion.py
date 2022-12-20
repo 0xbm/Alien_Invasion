3aa114f4bd48b474427a92cd3a6ed1b878499cf6
0xbm <0xmb@protonmail.com>
2022-12-20 19:31:46 +0100

    best scores

--- alien_invasion.py
+++ alien_invasion.py
@@ -155,9 +155,10 @@ class AlienInvasion:
 
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
         if collisions:
-            for alien in collisions.values():
+            for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
                 self.sb.prep_score()
+                self.sb.check_high_score()
 
         if not self.aliens:
             self.bullets.empty()
