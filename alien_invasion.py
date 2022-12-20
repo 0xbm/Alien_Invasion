caf894f91502746ce3653a524b304b33ae8a8c09
0xbm <0xmb@protonmail.com>
2022-12-20 19:39:52 +0100

    show level

--- alien_invasion.py
+++ alien_invasion.py
@@ -100,7 +100,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
-
+            self.sb.prep_level()
             self.aliens.empty()
             self.bullets.empty()
 
@@ -163,6 +163,10 @@ class AlienInvasion:
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
+
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
