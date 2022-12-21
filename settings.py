2e2fd201cbd9165add32ea6be14fb948d05635aa
0xbm <0xmb@protonmail.com>
2022-12-21 15:00:30 +0100

    update (change) bullets speed

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ class Settings:
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
-        self.bullet_speed = 1.5
+        self.bullet_speed = 3.0
         self.alien_speed = 1.0
         self.ship_speed = 1.5
 
