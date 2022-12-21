0400ea09ba48bae382eca10085db7e2393482b9a
0xbm <0xmb@protonmail.com>
2022-12-21 14:58:53 +0100

    fix bugs

--- alien.py
+++ alien.py
@@ -18,7 +18,7 @@ class Alien(Sprite):
 
     def check_edges(self):
         screen_rect = self.screen.get_rect()
-        if screen_rect.right >= screen_rect.right or self.rect.left <= 0:
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
 
     def update(self):
